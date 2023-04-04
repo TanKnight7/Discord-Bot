a0e42304422a15b7ea4afee5e6704517c43c950c
Tan Knight <49807077+TanKnight7@users.noreply.github.com>
2023-04-04 17:47:37 +0700

    Update apikeys.py

--- apikeys.py
+++ apikeys.py
@@ -1,5 +1,5 @@
-botToken = ""
-owner_id = int("")
-admin_channel_id = int("")
-member_join_id = int("")
-member_leave_id = int("")
+botToken = "BOT TOKEN HERE!"
+owner_id = int("YOUR DISCORD ID HERE!")
+admin_channel_id = int("YOUR MODERATION CHANNEL ID HERE!")
+member_join_id = int("YOUR JOIN NOTIFICATION CHANNEL ID HERE!")
+member_leave_id = int("YOUR LEAVE NOTIFICATION CHANNEL ID HERE!")
