0457494f7331828dda2a933df8fa379b601d6bdf
Tan Knight <49807077+TanKnight7@users.noreply.github.com>
2023-04-04 17:46:52 +0700

    Update apikeys.py

--- apikeys.py
+++ apikeys.py
@@ -1,5 +1,5 @@
-botToken = "MTA5MTk0MzgyMjYwNDM3NDA0Ng.GtIzoN.7G7bHXpbRBpvWUGZj2Mh4X_ctcRfMYHp54qA4E"
-owner_id = int("721017621335638071")
-admin_channel_id = int("1092439437604032534")
-member_join_id = int("756400874896621598")
-member_leave_id = int("1092440712135589939")
+botToken = ""
+owner_id = int("")
+admin_channel_id = int("")
+member_join_id = int("")
+member_leave_id = int("")
